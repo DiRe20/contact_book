27e028a61a064739abd6526905985546ad66d69b
Aurélio Faria <diogoaureliofaria@hotmail.com>
2024-11-10 22:52:50 +0000

    refactor layout

--- public/js/remove_contacts.js
+++ public/js/remove_contacts.js
@@ -1,4 +1,20 @@
 var listChecked = [];
+
+$(document).ready(function() {
+    $('#select_all_check').change(function() {
+        $('.line-js-contact').prop('checked', $(this).prop('checked'));
+    });
+
+    $('.line-js-contact').change(function() {
+        if (!$(this).prop('checked')) {
+            $('#select_all_check').prop('checked', false);
+        }
+        else if ($('.line-js-contact:checked').length === $('.line-js-contact').length) {
+            $('#select_all_check').prop('checked', true);
+        }
+    });
+});
+
 function checkedList() {
     $(':checkbox').each(function () {
         if (this.checked && this.className === 'line-js-contact') {
